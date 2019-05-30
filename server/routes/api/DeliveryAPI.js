e8528a2ee404b5edbde46f71fb3209786c4f0015
bankkeez <sethitikul@gmail.com>
2019-05-30 14:29:46 +0700

    remodel olap to sql

--- server/routes/api/DeliveryAPI.js
+++ server/routes/api/DeliveryAPI.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const mongodb = require("mongodb");
-const Olap = require("../backend/Olap.js");
-const olap = new Olap();
+const SQLCover = require("../backend/SQLCover");
+const sql = new SQLCover();
 const router = express.Router();
 var facts = "";
 
@@ -19,31 +19,31 @@ router.get("/data/:data", async (req, res) => {
 
 // Get requested average send Type.
 // /api/delivery/average/send-type
-router.get("/average/send-type", async (req, res) => {
-  const data = olap.getTotalSendType();
-  res.send(data);
-});
+// router.get("/average/send-type", async (req, res) => {
+//   const data = sql.getTotalSendType();
+//   res.send(data);
+// });
 
 // Get average send Type with requested month.
 // /api/delivery/average/send-type/<month>
 router.get("/average/send-type/:month", async (req, res) => {
-  const data = olap.getTotalSendType(req.params.month);
+  const data = sql.getTotalSendType(req.params.month);
   res.send(data);
 });
 
 // Get average weight with requested month.
 // /api/delivery/average/weight/<month>/<year>
-router.get("/average/weight/:month/:year", async (req, res) => {
-  const data = olap.getAVGWeightPerMonth(req.params.month,req.params.year);
+router.get("/average/weight/:month", async (req, res) => {
+  const data = sql.getAVGWeightPerMonth(req.params.month);
   res.send(data);
 });
 
 // Get average service per station.
 // /api/delivery/average/service
-router.get("/average/service", async (req, res) => {
-  const data = olap.getStationServices();
-  res.send(data);
-});
+// router.get("/average/service", async (req, res) => {
+//   const data = olap.getStationServices();
+//   res.send(data);
+// });
 
 router.get("/test", async (req, res) => {
   console.log("route get /test");
