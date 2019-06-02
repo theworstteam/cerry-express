0d7552cd33e3b54f14241f90245f1e2c0c88e1e1
Vichaphol Thamsuthikul <vichaphol.t@ku.th>
2019-06-03 00:32:56 +0700

    done displaying data

--- client/src/components/resource/ResourceHistogram.jsx
+++ client/src/components/resource/ResourceHistogram.jsx
@@ -5,18 +5,6 @@ import Title from "../Title";
 export default class ResourceHistogram extends Component {
 	constructor(props) {
 		super(props);
-		// console.log(this.props.histogram[0])
-		const {
-			Bangkok,
-			ChiangRai,
-			Chonburi,
-			Kanchanaburi,
-			Krabi,
-			Nan,
-			Nonthaburi,
-			Tak,
-			Yala,
-		} = this.props.histogram;
 		this.state = {
 			labels: [
 				"Bangkok",
@@ -32,23 +20,45 @@ export default class ResourceHistogram extends Component {
 			datasets: [
 				{
 					label: this.props.name,
-					data: [
-						Bangkok,
-						ChiangRai,
-						Chonburi,
-						Kanchanaburi,
-						Krabi,
-						Nan,
-						Nonthaburi,
-						Tak,
-						Yala,
-					],
+					data: [],
 					backgroundColor: "#F7D679",
 				},
 			],
 		};
 	}
 	render() {
+		const { histogram } = this.props;
+		let Bangkok = 0;
+		let ChiangRai = 0;
+		let Chonburi = 0;
+		let Kanchanaburi = 0;
+		let Krabi = 0;
+		let Nan = 0;
+		let Nonthaburi = 0;
+		let Tak = 0;
+		let Yala = 0;
+		for (let index = 0; index < histogram.length; index++) {
+			Bangkok += histogram[index].Bangkok;
+			ChiangRai += histogram[index].ChiangRai;
+			Chonburi += histogram[index].Chonburi;
+			Kanchanaburi += histogram[index].Kanchanaburi;
+			Krabi += histogram[index].Krabi;
+			Nan += histogram[index].Nan;
+			Nonthaburi += histogram[index].Nonthaburi;
+			Tak += histogram[index].Tak;
+			Yala += histogram[index].Yala;
+		}
+		this.state.datasets[0].data = [
+			Bangkok,
+			ChiangRai,
+			Chonburi,
+			Kanchanaburi,
+			Krabi,
+			Nan,
+			Nonthaburi,
+			Tak,
+			Yala,
+		];
 		return (
 			<div className='center-histogram'>
 				<Title name={this.props.title} />
--- client/src/pages/Resource.jsx
+++ client/src/pages/Resource.jsx
@@ -8,16 +8,22 @@ export default function Resource() {
 		<div>
 			<ParcelConsumer>
 				{value => {
-					const { loading, branch_car, branch_staff } = value;
-					console.log(branch_car);
-					console.log(branch_staff);
+					const { loading, branch_car, branch_staff} = value;
 					if (loading) {
 						return <Loading name='Resource' />;
 					}
 					return (
 						<div>
-							<ResourceHistogram histogram={branch_car[0]} name='total car per branch' title='Car' />
-							<ResourceHistogram histogram={branch_car[0]} name='total staff per branch' title='Staff' />
+							<ResourceHistogram
+								histogram={branch_car}
+								name='total car per branch'
+								title='Car'
+							/>
+							<ResourceHistogram
+								histogram={branch_staff}
+								name='total staff per branch'
+								title='Staff'
+							/>
 						</div>
 					);
 				}}
